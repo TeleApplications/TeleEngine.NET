0f834a822bfe88d163123955dc54155263c970e9
Ma-tes <msestaubr@gmail.com>
2022-10-12 22:10:32 +0200

    Created uncomplete start of loading vertices from different files

---
+++ source/TeleEngine.NET/Models/Interfaces/IFormat.cs
@@ -0,0 +1,11 @@
+﻿namespace TeleEngine.NET.Models.Interfaces
+{
+    public interface IFormat
+    {
+        public string Name { get; }
+
+        public ReadOnlyMemory<float> LoadData(string path);
+
+        public bool ValideteFormat(string path);
+    }
+}
